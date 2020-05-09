00cc54734914e9a0e733ea103b179c3a132ae36b
xBiggs <42427630+xBiggs@users.noreply.github.com>
2020-05-09 08:27:21 -0400

    Added commands files

---
+++ commands/characters.ts
@@ -0,0 +1,159 @@
+import { Character, TECharacter } from '../classes/cCharacter';
+
+// MK9 Characters
+const baraka: Character = new Character('Baraka');
+const csz: Character = new Character('CSZ');
+const cyrax: Character = new Character('Cyrax');
+const ermac: Character = new Character('Ermac');
+const freddy: Character = new Character('Freddy');
+const jade: Character = new Character('Jade');
+const jax: Character = new Character('Jax');
+const johnnycage: Character = new Character('Johnny Cage');
+const kabal: Character = new Character('Kabal');
+const kano: Character = new Character('Kano');
+const kenshi: Character = new Character('Kenshi');
+const kitana: Character = new Character('Kitana');
+const kratos: Character = new Character('Kratos');
+const kunglao: Character = new Character('Kung Lao');
+const liukang: Character = new Character('Liu Kang');
+const mileena: Character = new Character('Mileena');
+const nightwolf: Character = new Character('Nightwolf');
+const noobsaibot: Character = new Character('Noob Saibot');
+const quanchi: Character = new Character('Quan Chi');
+const raiden: Character = new Character('Raiden');
+const rain: Character = new Character('Rain');
+const reptile: Character = new Character('Reptile');
+const scorpion: Character = new Character('Scorpion');
+const sektor: Character = new Character('Sektor');
+const shangtsung: Character = new Character('Shang Tsung');
+const sheeva: Character = new Character('Sheeva');
+const sindel: Character = new Character('Sindel');
+const skarlet: Character = new Character('Skarlet');
+const smoke: Character = new Character('Smoke');
+const sonya: Character = new Character('Sonya');
+const stryker: Character = new Character('Stryker');
+const subzero: Character = new Character('Sub-Zero');
+
+export const MK9Characters = {
+    baraka: baraka,
+    csz: csz,
+
+    cyrax: cyrax,
+    rax: cyrax,
+
+    ermac: ermac,
+    mac: ermac,
+
+    freddy: freddy,
+    jade: jade,
+    jax: jax,
+
+    johnnycage: johnnycage,
+    johnny: johnnycage,
+    cage: johnnycage,
+    jc: johnnycage,
+
+    kabal: kabal,
+    god: kabal,
+
+    kano: kano,
+    kenshi: kenshi,
+    scrub: kenshi,
+
+    kitana: kitana,
+    kit: kitana,
+
+    kratos: kratos,
+
+    kunglao: kunglao,
+    kung: kunglao,
+    lao: kunglao,
+    kl: kunglao,
+
+    liukang: liukang,
+    liu: liukang,
+    kang: liukang,
+    lk: liukang,
+
+    mileena: mileena,
+    mil: mileena,
+
+    nightwolf: nightwolf,
+    nw: nightwolf,
+    wolf: nightwolf,
+
+    noobsaibot: noobsaibot,
+    noob: noobsaibot,
+    saibot: noobsaibot,
+
+    quanchi: quanchi,
+    quan: quanchi,
+
+    raiden: raiden,
+    rain: rain,
+
+    reptile: reptile,
+    rep: reptile,
+
+    scorpion: scorpion,
+    scorp: scorpion,
+
+    sektor: sektor,
+
+    shangtsung: shangtsung,
+    shang: shangtsung,
+
+    sheeva: sheeva,
+
+    sindel: sindel,
+    sin: sindel,
+    sexdel: sindel,
+
+    skarlet: skarlet,
+    skar: skarlet,
+    scarlet: skarlet,
+    scar: skarlet,
+
+    smoke: smoke,
+    gay: smoke,
+
+    sonya: sonya,
+    stryker: stryker,
+
+    subzero: subzero,
+    sub: subzero
+}
+
+// // TE Characters
+// export const tebaraka: TEBaraka = new TEBaraka();
+// export const tecsz: TECSZ = new TECSZ();
+// export const tecyrax: TECyrax = new TECyrax();
+// export const teermac: TEErmac = new TEErmac();
+// export const tefreddy: TEFreddy = new TEFreddy();
+// export const tejade: TEJade = new TEJade();
+// export const tejax: TEJax = new TEJax();
+// export const tejohnnycage: TEJohnnyCage = new TEJohnnyCage();
+// export const tekabal: TEKabal = new TEKabal();
+// export const tekano: TEKano = new TEKano();
+// export const tekenshi: TEKenshi = new TEKenshi();
+// export const tekitana: TEKitana = new TEKitana();
+// export const tekratos: TEKratos = new TEKratos();
+// export const tekunglao: TEKungLao = new TEKungLao();
+// export const teliukang: TELiuKang = new TELiuKang();
+// export const temileena: TEMileena = new TEMileena();
+// export const tenightwolf: TENightwolf = new TENightwolf();
+// export const tenoobsaibot: TENoobSaibot = new TENoobSaibot();
+// export const tequanchi : TEQuanChi = new TEQuanChi();
+// export const teraiden: TERaiden = new TERaiden();
+// export const terain: TERain = new TERain();
+// export const tereptile: TEReptile = new TEReptile();
+// export const tescorpion: TEScorpion = new TEScorpion();
+// export const tesektor: TESektor = new TESektor();
+// export const teshangtsung: TEShangTsung = new TEShangTsung();
+// export const tesheeva: TESheeva = new TESheeva();
+// export const tesindel: TESindel = new TESindel();
+// export const teskarlet: TESkarlet = new TESkarlet();
+// export const tesmoke: TESmoke = new TESmoke();
+// export const tesonya: TESonya = new TESonya();
+// export const testryker: TEStryker = new TEStryker();
+// export const tesubzero: TESubZero = new TESubZero();
