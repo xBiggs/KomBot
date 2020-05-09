66851efb334f1bb1e003a036c3fcd01cc259e7c7
xBiggs <42427630+xBiggs@users.noreply.github.com>
2020-05-09 08:24:42 -0400

    Added iFrameData.ts

---
+++ interfaces/iFrameData.ts
@@ -0,0 +1,13 @@
+export interface FrameData {
+    command: string,
+    name: string,
+    execution: string,
+    startup: string,
+    level: string,
+    block: string,
+    hit: string,
+    blockCancel: string,
+    hitCancel: string,
+    notes: string,
+    image: string,
+}
