bfcc9ac3684f57caed85557de0387a71c169015c
xBiggs <biggs@biggs.xyz>
2023-05-21 21:43:14 -0400

    Finish Rewrite

---
+++ src/alias.ts
@@ -0,0 +1,124 @@
+export const Alias = new Map<string, string>([
+  ['baraka', 'baraka'],
+  ['csz', 'csz'],
+
+  ['cyrax', 'cyrax'],
+  ['rax', 'cyrax'],
+
+  ['ermac', 'ermac'],
+  ['mac', 'ermac'],
+
+  ['freddy', 'freddy'],
+  ['jade', 'jade'],
+  ['jax', 'jax'],
+
+  ['johnnycage', 'johnnycage'],
+  ['johnny', 'johnnycage'],
+  ['cage', 'johnnycage'],
+  ['jc', 'johnnycage'],
+
+  ['kabal', 'kabal'],
+  ['god', 'kabal'],
+
+  ['kano', 'kano'],
+  ['kenshi', 'kenshi'],
+  ['scrub', 'kenshi'],
+
+  ['kitana', 'kitana'],
+  ['kit', 'kitana'],
+
+  ['kratos', 'kratos'],
+
+  ['kunglao', 'kunglao'],
+  ['kung', 'kunglao'],
+  ['lao', 'kunglao'],
+  ['kl', 'kunglao'],
+
+  ['liukang', 'liukang'],
+  ['liu', 'liukang'],
+  ['kang', 'liukang'],
+  ['lk', 'liukang'],
+
+  ['mileena', 'mileena'],
+  ['mil', 'mileena'],
+
+  ['nightwolf', 'nightwolf'],
+  ['nw', 'nightwolf'],
+  ['wolf', 'nightwolf'],
+
+  ['noobsaibot', 'noobsaibot'],
+  ['noob', 'noobsaibot'],
+  ['saibot', 'noobsaibot'],
+
+  ['quanchi', 'quanchi'],
+  ['quan', 'quanchi'],
+
+  ['raiden', 'raiden'],
+  ['rain', 'rain'],
+
+  ['reptile', 'reptile'],
+  ['rep', 'reptile'],
+
+  ['scorpion', 'scorpion'],
+  ['scorp', 'scorpion'],
+
+  ['sektor', 'sektor'],
+
+  ['shangtsung', 'shangtsung'],
+  ['shang', 'shangtsung'],
+
+  ['sheeva', 'sheeva'],
+
+  ['sindel', 'sindel'],
+  ['sin', 'sindel'],
+  ['sexdel', 'sindel'],
+
+  ['skarlet', 'skarlet'],
+  ['skar', 'skarlet'],
+  ['scarlet', 'skarlet'],
+  ['scar', 'skarlet'],
+
+  ['smoke', 'smoke'],
+  ['gay', 'smoke'],
+
+  ['sonya', 'sonya'],
+  ['stryker', 'stryker'],
+
+  ['subzero', 'subzero'],
+  ['sub', 'subzero'],
+]);
+
+export const Fighter = new Map<string, string>([
+  ['baraka', 'Baraka'],
+  ['csz', 'CSZ'],
+  ['cyrax', 'Cyrax'],
+  ['ermac', 'Ermac'],
+  ['freddy', 'Freddy'],
+  ['jade', 'Jade'],
+  ['jax', 'Jax'],
+  ['johnnycage', 'Johnny Cage'],
+  ['kabal', 'Kabal'],
+  ['kano', 'Kano'],
+  ['kenshi', 'Kenshi'],
+  ['kitana', 'Kitana'],
+  ['kratos', 'Kratos'],
+  ['kunglao', 'Kung Lao'],
+  ['liukang', 'Liu Kang'],
+  ['mileena', 'Mileena'],
+  ['nightwolf', 'Nightwolf'],
+  ['noobsaibot', 'Noob Saibot'],
+  ['quanchi', 'Quan Chi'],
+  ['raiden', 'Raiden'],
+  ['rain', 'Rain'],
+  ['reptile', 'Reptile'],
+  ['scorpion', 'Scorpion'],
+  ['sektor', 'Sektor'],
+  ['shangtsung', 'Shang Tsung'],
+  ['sheeva', 'Sheeva'],
+  ['sindel', 'Sindel'],
+  ['skarlet', 'Skarlet'],
+  ['smoke', 'Smoke'],
+  ['sonya', 'Sonya'],
+  ['stryker', 'Stryker'],
+  ['subzero', 'Sub-Zero'],
+]);
